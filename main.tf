f67f75ac307805be1070d5b600ab5be014631b4b
andremarciolopes <88516504+andremarciolopes@users.noreply.github.com>
2021-10-31 15:44:03 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region = "us-east-2"
 }
-resource "aws_instance" "maquina_windows_iis" {
+resource "aws_instance" "maquina_ubuntu_server" {
   ami = "ami-00399ec92321828f5"
   instance_type = "t2.micro"
   key_name = "terraform-key"
   tags = {
-    Name = "maquina_ansible_com_iis"
+    Name = "maquina_ansible_ubuntu"
   } 
   vpc_security_group_ids = ["${aws_security_group.acesso_geral_11.id}"]
 }
