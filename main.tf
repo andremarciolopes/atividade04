f9be838e6e965ef840909654180689986e4515a9
andremarciolopes <88516504+andremarciolopes@users.noreply.github.com>
2021-10-31 13:48:05 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_instance" "maquina_wp" {
-  ami = "#{vars.amis["us-east-2-windows-server-2019"]}"
-  instance_type = "#{vars.instance_type.micro}"
-  key_name = "terraform-key
+  ami = "#{var.amis["us-east-2-windows-server-2019"]}"
+  instance_type = "#{var.instance_type.micro}"
+  key_name = "terraform-key"
   tags = {
     Name = "maquina_ansible_com_iis"
   } 
