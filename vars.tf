5275bb41eb29e6a7f175e39af91cac6075cc8016
andremarciolopes <88516504+andremarciolopes@users.noreply.github.com>
2021-10-31 13:21:56 -0300

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,1 +1,10 @@
-
+variable "amis" {
+  default = {
+    "us-east-2-windows-server-2019" = "ami-08c213ebdf7b857c2"
+  }
+}
+variable "instance_type" {
+  default = {
+    "micro" = "t2.micro"
+  }
+}
