8eba7e5edba96dc4ec29c96420defd6d3534e4c5
andremarciolopes <88516504+andremarciolopes@users.noreply.github.com>
2021-10-31 13:58:43 -0300

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -2,33 +2,37 @@ resource "aws_security_group" "acesso_geral" {
   name        = "acesso_geral"
   description = "Atividade04"
 
-  ingress = [{
-    cidr_blocks      = [
-      "0.0.0.0/0",
-    ]
-    description      = "Para acessar a máquina com todas as portas"
-    from_port        = 0
-    ipv6_cidr_blocks = "::/0",
-    prefix_list_ids  = []
-    protocol         = "-1"
-    security_groups  = []
-    self             = false
-    to_port          = 0
-  }] 
+  ingress = [
+    {
+      cidr_blocks      = [
+        "0.0.0.0/0",
+      ]
+      description      = "Para acessar a máquina com todas as portas"
+      from_port        = 0
+      ipv6_cidr_blocks = "::/0",
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
   
-  egress = {
-    cidr_blocks      = [
-      "0.0.0.0/0",
-    ]
-    description      = "Para acessar a máquina com todas as portas"
-    from_port        = 0
-    ipv6_cidr_blocks = "::/0",
-    prefix_list_ids  = []
-    protocol         = "-1"
-    security_groups  = []
-    self             = false
-    to_port          = 0
-  }    
+  egress = [
+    {
+      cidr_blocks      = [
+        "0.0.0.0/0",
+      ]
+      description      = "Para acessar a máquina com todas as portas"
+      from_port        = 0
+      ipv6_cidr_blocks = "::/0",
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }    
+  ]
   
 
   tags = {
