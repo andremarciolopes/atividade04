3ab6b361dbc1d63f1e145a5d931ad6aefbfb8821
andremarciolopes <88516504+andremarciolopes@users.noreply.github.com>
2021-10-31 14:15:57 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "maquina_iis" {
   ami = "ami-08c213ebdf7b857c2"
-  instance_type = "#{vars.instance_type.micro}"
+  instance_type = "t2.micro"
   key_name = "terraform-key"
   tags = {
     Name = "maquina_ansible_com_iis"
